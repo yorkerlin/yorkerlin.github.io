dfbde207ecb10c3c6608cd2351fb2ae25cd3b6c8
GitHub Action <action@github.com>
2022-04-12 07:48:14 +0000

    Build 4cc990f4fa

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -70,7 +70,7 @@ var store = [{
         "url": "/posts/2021/10/Geomopt02/",
         "teaser":null},{
         "title": "Part III: Invariance of Natural-Gradients",
-        "excerpt":"Warning: working in Progress (incomplete) Goal This blog post focuses on the invariance of natural-gradients. We will discuss transformation rules related to natural-gradients and how to compute natural-gradients in a new coordinate system via Automatic Differentiation. However, the Euclidean counterpart in general can be non-invariant, which is the key reason...","categories": [],
+        "excerpt":"Goal This blog post focuses on the invariance of natural-gradients.We will discuss transformation rules related to natural-gradients and the computation via Automatic Differentiation. the non-invariance of the Euclidean counterpart, which is the key reason to explain why Euclidean gradient descent is not invariant. The discussion here is informal and focuses...","categories": [],
         "tags": ["Natural Gradient Descent","Information Geometry","Riemannian Manifold"],
         "url": "/posts/2021/11/Geomopt03/",
         "teaser":null},{
